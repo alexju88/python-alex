eea8fde499785db9b051074efa6edb8fd116d3fb
Alex Ju <alex.ju@cn.bosch.com>
2019-09-26 10:22:34 +0800

    Update get_public_ip.py

--- get_public_ip.py
+++ get_public_ip.py
@@ -1,13 +1,15 @@
+#Python3
 #from bs4 import BeautifulSoup
+import json
 from urllib.request import urlopen
 
-my_ip = load(urlopen('http://jsonip.com'))['ip']
+my_ip = json.load(urlopen('http://jsonip.com'))['ip']
 print(my_ip)
 
 my_ip = urlopen('http://ip.42.pl/raw').read() #bytes value
 print(str(my_ip)) #transfer to string
 
 #slow 
-my_ip = load(urlopen('https://api.ipify.org/?format=json'))['ip']
+my_ip = json.load(urlopen('https://api.ipify.org/?format=json'))['ip']
 print(my_ip)
 
